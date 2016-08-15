5ee83bf4eb2832039d16ffbbd2d56972028add82
Jose Santiago <jose@gallifrey.local>
2016-08-14 22:39:18 -0400

    Added comments to start.sh

--- start.sh
+++ start.sh
@@ -1,5 +1,9 @@
 # Clean up any unreleased virtual frame buffer instances and prepare the new one
 rm /tmp/.X9-lock
+
+# Create new virtual frame buffer with X-Window environment
 Xvfb -ac -screen scrn 1280x960x24 :9.0 &
 export DISPLAY=:9.0
+
+# Start the Node-Express microservice
 npm start
